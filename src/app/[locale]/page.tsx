c5cb8b0f8f4984aa4a427d85fc1600ba414b8d26
RamiroHR <19.rodriguez.89@gmail.com>
2025-05-02 16:18:31 +0200

    chore: update home page & rename api route 'register' as 'signup'

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,15 +4,18 @@ import {Link} from '@/i18n/navigation';
 export default function HomePage() {
   const t = useTranslations('HomePage');
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center gap-4">
-      <h1>{t('title')}</h1>
-      <p>{t('subtitle')}</p>
-      <button className="border rounded bg-gray-600 text-white px-4 py-2 hover:bg-gray-700">
-        {t('cta')}
-      </button>
-      <img src="./astronaut.png" width={200}/>
-      <Link href="/about" className="text-gray-500">
-        {t('about')}
+    <div className="min-h-screen flex flex-col items-center justify-center gap-2">
+      <h1 className="block text-3xl text-blue-600 font-bold italic">
+        {t('title')}
+      </h1>
+      <p className="block text-m text-gray-300 italic">
+        {t('subtitle')}
+      </p>
+      <img className="mt-4" src="./astronaut.png" width={200}/>
+      <Link href="/login" className="text-gray-500">
+        <button className="bg-sky-800/50 border border-sky-900 rounded text-white px-4 py-2 hover:bg-sky-700/50">
+          {t('cta')}
+        </button>
       </Link>
     </div>
   );
