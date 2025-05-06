ebfe0dadf44a2c728f164a0fb22fdd43f41d27e7
RamiroHR <19.rodriguez.89@gmail.com>
2025-05-06 13:17:09 +0200

    chore: use <Image> instead of <img> for optimized image rendering

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,6 @@
 import { useTranslations } from 'next-intl';
 import { Link } from '@/i18n/navigation';
+import Image from 'next/image';
 
 // Define locale-specific Home Page
 export default function HomePage() {
@@ -12,7 +13,14 @@ export default function HomePage() {
       <p className="block text-m text-gray-300 italic">
         {t('subtitle')}
       </p>
-      <img className="mt-4" src="./astronaut.png" width={200}/>
+      <Image
+        className="mt-4 w-full max-w-[200px] h-auto"
+        src="/astronaut.png"
+        width={200}
+        height={200}
+        alt="astronaut"
+        sizes="(max-width: 100px) 100vw, 200px"
+      />
       <Link href="/login" className="text-gray-500">
         <button className="bg-sky-800/50 border border-sky-900 rounded text-white px-4 py-2 hover:bg-sky-700/50">
           {t('cta')}
