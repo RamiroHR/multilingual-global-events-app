9d0fd9576a5bb94f259188ed604f156d8b171609
RamiroHR <19.rodriguez.89@gmail.com>
2025-05-07 18:18:20 +0200

    test: add sigunp unit test (to expand)

---
+++ tests/api/signup.test.ts
@@ -0,0 +1,56 @@
+import { POST } from "@/app/api/auth/signup/route";
+import { NextRequest } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { findUserByEmail } from "@/lib/user";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env.test" });
+
+// helper function to simulate the request body to send to the endpoint
+type SignupRequestBody = {
+  email: string;
+  username: string;
+  password: string;
+};
+
+const createMockRequest = (body: SignupRequestBody) =>
+  ({ json: async () => body }) as unknown as NextRequest;
+
+describe("POST /api/auth/signup", () => {
+  // Initial & Final Cleanup
+  beforeEach(async () => {
+    await prisma.password.deleteMany({});
+    await prisma.user.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await prisma.password.deleteMany({});
+    await prisma.user.deleteMany({});
+    await prisma.$disconnect();
+  });
+
+  it("should create a new user with valid data", async () => {
+    const mockUserData = {
+      email: "test@example.com",
+      username: "testuser",
+      password: "pasword123",
+    };
+
+    // test logic
+    const req = createMockRequest(mockUserData);
+    const response = await POST(req);
+    const data = await response.json();
+
+    // response assertions
+    expect(response.status).toBe(200);
+    expect(data).toHaveProperty("id");
+    expect(data.email).toBe(mockUserData.email);
+    expect(data.username).toBe(mockUserData.username);
+    expect(data).not.toHaveProperty("password");
+
+    // database verification
+    const createdUser = await findUserByEmail(mockUserData.email);
+    expect(createdUser).toBeTruthy();
+    expect(createdUser?.email).toBe(mockUserData.email);
+    expect(createdUser?.username).toBe(mockUserData.username);
+  });
+});
