e2db77c24ecdf2f97c56874801eb0d423e2c2cf2
RamiroHR <19.rodriguez.89@gmail.com>
2025-05-07 19:47:54 +0200

    test: expand unit tests for signup route

--- tests/api/signup.test.ts
+++ tests/api/signup.test.ts
@@ -3,6 +3,7 @@ import { NextRequest } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { findUserByEmail } from "@/lib/user";
 import dotenv from "dotenv";
+import { mock } from "node:test";
 dotenv.config({ path: "./.env.test" });
 
 // helper function to simulate the request body to send to the endpoint
@@ -28,6 +29,7 @@ describe("POST /api/auth/signup", () => {
     await prisma.$disconnect();
   });
 
+  // tests
   it("should create a new user with valid data", async () => {
     const mockUserData = {
       email: "test@example.com",
@@ -52,5 +54,68 @@ describe("POST /api/auth/signup", () => {
     expect(createdUser).toBeTruthy();
     expect(createdUser?.email).toBe(mockUserData.email);
     expect(createdUser?.username).toBe(mockUserData.username);
+  });
+
+  it("should return 400 for missing fields", async () => {
+    // mosck data
+    const testCases = [
+      { email: "test@example.com", username: "testuser" },
+      { email: "test@example.com", password: "password123" },
+      { username: "testuser", password: "password123" },
+      {}, // missing all fields
+    ];
+
+    // test logic
+    for (const testCase of testCases) {
+      const req = createMockRequest(testCase);
+      const response = await POST(req);
+      const data = await response.json();
+
+      // response assertions
+      expect(response.status).toBe(400);
+      expect(data.error).toBe("Missing fields");
+    }
+  });
+
+  it("should return 409 for existing email", async () => {
+    // mock data
+    const mockUserData = {
+      email: "test@example.com",
+      username: "testuser",
+      password: "pasword123",
+    };
+
+    // test logic
+    const req1 = createMockRequest(mockUserData);
+    await POST(req1);
+
+    const req2 = createMockRequest({
+      ...mockUserData,
+      username: "differentUser",
+    });
+    const response = await POST(req2);
+    const data = await response.json();
+
+    // response assertions
+    expect(response.status).toBe(409);
+    expect(data.error).toBe("Email already in use");
+  });
+
+  it("should hash the password before storing", async () => {
+    // mock data
+    const mockUserData = {
+      email: "test@example.com",
+      username: "testuser",
+      password: "password123",
+    };
+
+    // test logic
+    const req = createMockRequest(mockUserData);
+    await POST(req);
+
+    // DB verifications
+    const createdUser = await findUserByEmail(mockUserData.email);
+    expect(createdUser?.password?.password).not.toBe(mockUserData.email);
+    expect(createdUser?.password?.password).toMatch(/^\$2[aby]\$\d+\$/); // matches the bcrypt format hash
   });
 });
