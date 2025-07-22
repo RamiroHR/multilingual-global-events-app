31517926f3da10b6ccdf34c2abdc90739dd9b738
RamiroHR <19.rodriguez.89@gmail.com>
2025-07-22 18:08:43 +0200

    chore: add baseQuery handling authorization header. Update countriesApi and eventsApi (DRY principle)

--- src/redux/services/countriesApi/countriesApi.ts
+++ src/redux/services/countriesApi/countriesApi.ts
@@ -1,20 +1,12 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi } from "@reduxjs/toolkit/query/react";
 import ROUTES from "@/lib/routes/routes";
 import { CountriesEntity } from "./countriesApi.types";
+import { baseQuery } from "@/redux/services/baseQuery";
 
 // create and export service
 export const countriesApi = createApi({
   reducerPath: "countriesApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "",
-    prepareHeaders: (headers) => {
-      const token = localStorage.getItem("token");
-      if (token) {
-        headers.set("Authorization", `Bearer ${token}`);
-      }
-      return headers;
-    },
-  }),
+  baseQuery: baseQuery,
   endpoints: (builder) => ({
     getCountries: builder.query<CountriesEntity, void>({ query: () => ROUTES.ALL_COUNTRIES }),
   }),
--- src/redux/services/eventsApi/eventsApi.ts
+++ src/redux/services/eventsApi/eventsApi.ts
@@ -1,20 +1,12 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi } from "@reduxjs/toolkit/query/react";
 import { UpcomingEventsEntity, UpcomingEventsQuery } from "./eventsApi.types";
 import ROUTES from "@/lib/routes/routes";
+import { baseQuery } from "@/redux/services/baseQuery";
 
 // service
 export const eventsApi = createApi({
   reducerPath: "eventsApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "",
-    prepareHeaders: (headers) => {
-      const token = localStorage.getItem("token");
-      if (token) {
-        headers.set("Authorization", `Bearer ${token}`);
-      }
-      return headers;
-    },
-  }),
+  baseQuery: baseQuery,
   endpoints: (builder) => ({
     getUpcomingEvents: builder.query<UpcomingEventsEntity, UpcomingEventsQuery>({
       query: ({ page, onlineOnly, country }) =>
